c2ab1b11772ee4bd0518e221debef923e584fe32
Stephenr816 <stephenr816@gmail.com>
2021-02-09 21:48:33 +0530

    latest update

--- Object Repository/General Settings/Verify Where to Include Reviews/Page_Movie page  Starcat Testing/div_Pros  ConsGutHatHutterPoor Movie.rs
+++ Object Repository/General Settings/Verify Where to Include Reviews/Page_Movie page  Starcat Testing/div_Pros  ConsGutHatHutterPoor Movie.rs
@@ -6,15 +6,19 @@
    <elementGuidId>afd55610-ba15-46ed-91db-adc188b8d870</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>XPATH</key>
+         <value>//article[@id='post-99']/div/div/div/div[3]</value>
+      </entry>
+      <entry>
          <key>CSS</key>
          <value>div.prosandcons</value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//article[@id='post-99']/div/div/div/div[3]</value>
+         <key>BASIC</key>
+         <value>//*[@class = 'prosandcons']</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -24,14 +28,14 @@
       <value>div</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>class</name>
       <type>Main</type>
       <value>prosandcons</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
